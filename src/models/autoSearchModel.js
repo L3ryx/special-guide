fe5966ecb44d33ed7c9be36fe8c969d104cf8703
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-03-20 23:00:48 +0100

    Update autoSearchModel.js

--- src/models/autoSearchModel.js
+++ src/models/autoSearchModel.js
@@ -6,6 +6,7 @@ const autoSearchStateSchema = new mongoose.Schema({
   usedKeywords: { type: [String], default: [] },
   usedShops:    { type: [String], default: [] },
   etsyToken:    { type: String, default: null },
+  etsyEmail:    { type: String, default: null },
   updatedAt:    { type: Date, default: Date.now },
 });
 
