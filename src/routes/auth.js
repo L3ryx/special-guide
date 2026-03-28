e3d10b2ead57697c5ff86c9d9fe5ce0b8073c7fb
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-03-28 22:28:58 +0100

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -113,7 +113,7 @@ router.get('/etsy/callback', async (req, res) => {
     // Récupérer le profil Etsy pour avoir l'email / user_id
     const profileRes = await axios.get('https://api.etsy.com/v3/application/users/me', {
       headers: {
-        'x-api-key':     ETSY_CLIENT_ID,
+        'x-api-key':     ETSY_CLIENT_ID + ':' + ETSY_CLIENT_SECRET,
         'Authorization': 'Bearer ' + access_token,
       },
       timeout: 10000,
