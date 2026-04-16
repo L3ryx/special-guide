f2a8d50b44e57403f643c3b4f69595de14621427
L3ryx <94659000+L3ryx@users.noreply.github.com>
2026-04-16 08:09:13 +0200

    Delete src/services/imgbbUploader.js

--- src/services/imgbbUploader.js
+++
@@ -1,7 +0,0 @@
-// Les images Etsy (i.etsystatic.com) sont publiques — on les passe directement à Serper.
-// Plus besoin d'ImgBB ni de proxy pour Google Lens.
-function uploadToImgBB(input) {
-  return Promise.resolve(input);
-}
-
-module.exports = { uploadToImgBB };
